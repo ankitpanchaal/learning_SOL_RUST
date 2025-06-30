a105b7a24f4459f45be9fc0fcad72196b9ae57a3
ankitpanchaal <ankit0panchal@gmail.com>
2025-06-30 19:10:24 +0530

    ready to depl

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use axum::{
 };
 use tower_http::cors::CorsLayer;
 use tokio::net::TcpListener;
+use std::env;
 
 #[tokio::main]
 async fn main() {
@@ -19,8 +20,12 @@ async fn main() {
         .route("/api/transfer", post(api::handlers::transfer))
         .layer(CorsLayer::permissive());
 
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    println!("Server running on http://127.0.0.1:3000");
+    // Get port from environment or default to 3000
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let bind_address = format!("0.0.0.0:{}", port);
+    
+    let listener = TcpListener::bind(&bind_address).await.unwrap();
+    println!("Server running on http://{}", bind_address);
     
     axum::serve(listener, app)
         .await
